ce92cf1d6fda2e921f0f45cb86e9e8f6baba343c
Elias-Olofsson <54439741+Elias-Olofsson@users.noreply.github.com>
2019-09-27 09:57:20 +0200

    uwu

---
+++ metod-1/metod-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace metod_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radie = 2.4;
+            Console.WriteLine(Omkrets(radie));
+            Console.WriteLine(Area(radie));
+        }
+
+        static double Omkrets(double radie)
+        {
+            double omkrets = radie * 2 * Math.PI;
+            return omkrets;
+        }
+
+        static double Area(double radie)
+        {
+            double area = radie * radie * Math.PI;
+            return area;
+        }
+    }
+}
