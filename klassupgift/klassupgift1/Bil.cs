fbce5bd5dee0a5d593d20358064cce9330f12bca
Elias-Olofsson <54439741+Elias-Olofsson@users.noreply.github.com>
2019-12-17 14:05:41 +0100

    Add files via upload

---
+++ klassupgift/klassupgift1/Bil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace klassupgift1
+{
+	class Bil
+	{
+		private double maxHastighet;
+		private int styrka;
+		private string namn;
+		public void SetMaxHastighet(double m) {maxHastighet=m;}
+		public void SetStyrka(int s) {styrka = s;}
+		public void SetNamn(string n) {namn = n;}
+		public double GetMaxHastighet() {return maxHastighet; }
+		public int GetStyrka() {return styrka; }
+		public string GetNamn() {return namn; }
+
+	}
+}
---
+++ klassupgift/klassupgift1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace klassupgift1
+{
+    class Program
+    {
+        static void Main()
+        {
+			Bil fordon1 = new Bil();
+			Bil fordon2 = new Bil();
+
+			fordon1.SetNamn("skruttBil");
+			fordon1.SetMaxHastighet(12);
+			fordon1.SetStyrka(1);
+			fordon2.SetNamn("hyfsadBil");
+			fordon2.SetMaxHastighet(180);
+			fordon2.SetStyrka(6);
+
+			List<Bil> listOfBilar = new List<Bil>();
+			PrintIt(listOfBilar);
+		}
+
+		static void PrintIt(List<Bil> objList)
+		{
+			foreach (Bil item in objList)
+			{
+				Console.WriteLine(item.GetNamn());
+				Console.WriteLine(item.GetStyrka());
+				Console.WriteLine(item.GetMaxHastighet());
+			}
+		}
+		
+    }
+}
